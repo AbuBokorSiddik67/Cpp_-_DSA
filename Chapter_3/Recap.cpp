a1859717ba5764677ceb226e868b04d60b550247
AbuBokorSiddik67 <abubokorsiddik1238@gmail.com>
2025-09-11 09:17:24 +0600

    javascript recap

---
+++ Chapter_3/Recap.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a=10;
+//     cout<< "Hello world"<<endl<<"World\n";
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    cout<<"Hello World"<<endl;
+
+    return 0;
+}
