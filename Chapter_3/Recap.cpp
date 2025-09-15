a711e5b1f1bf528e2b58384f4df25fecfc1acd24
AbuBokorSiddik67 <abubokorsiddik1238@gmail.com>
2025-09-15 11:16:38 +0600

    True added

--- Chapter_3/Recap.cpp
+++ Chapter_3/Recap.cpp
@@ -10,7 +10,33 @@
 using namespace std;
 int main(){
 
-    cout<<"Hello World"<<endl;
+    // int a,b,c,d,e;
+    // cin>>a>>b>>c>>d>>e;
+    // if(a<b){
+    //     cout<<"A is lessthen B"<<endl;
+    // }
+    // else if(a<c && a<d && a<e){
+    //     cout<<"A is grater then all";
+    // }else{
+    //     cout<<"not is grater than";
+    // }
+
+    // for (int i=0; i<=2; i++){
+    //     cout<<i<<endl;
+    // }
+
+    // int i=0;
+    // while (i<=5)
+    // {
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+
+    int i, j;
+    for(i=1,j=2; i<=5,j<=6; i++,j++){
+        cout<<i<<j<<endl;
+    }
+    
 
     return 0;
 }
